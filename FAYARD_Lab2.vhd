8f51b7b93ab18ce5cec8790a3f247c7c8d3513ee
JessicaFyrd <jessica.fayard1993@laposte.net>
2023-01-11 14:53:31 +0100

    Compteur manuelle affiché sur 3 7-segment

--- FAYARD_Lab2.vhd
+++ FAYARD_Lab2.vhd
@@ -5,9 +5,11 @@ use IEEE.numeric_std.all;
 
 ENTITY FAYARD_Lab2 IS
    PORT(
-      clk	 : in  std_logic;
-      reset	 : in  std_logic;
-		output : out std_logic_vector(7 downto 0)
+      clk	  : in  std_logic;
+      reset	  : in  std_logic;
+		output0 : out std_logic_vector(7 downto 0);
+		output1 : out std_logic_vector(7 downto 0);
+		output2 : out std_logic_vector(7 downto 0)
       );
 END FAYARD_Lab2;
 
@@ -17,7 +19,7 @@ ARCHITECTURE ARCH_FAYARD_Lab2 OF FAYARD_Lab2 IS
 component FAYARD_Lab2_sys is
   port (
 		clk_clk                          : in  std_logic                    := 'X'; -- clk
-		pio_0_external_connection_export : out std_logic_vector(3 downto 0);        -- export
+		pio_0_external_connection_export : out std_logic_vector(11 downto 0);        -- export
 		reset_reset_n                    : in  std_logic                    := 'X'  -- reset_n
   );
 end component FAYARD_Lab2_sys;
@@ -29,7 +31,7 @@ component bin_to_7seg is
   );
 end component bin_to_7seg;
 
-signal internal_sig : std_logic_vector(3 downto 0);
+signal internal_sig : std_logic_vector(11 downto 0);
 
 begin
 	u0 : component FAYARD_Lab2_sys
@@ -39,10 +41,23 @@ begin
 		reset_reset_n                    => reset                     
   );
   
-   sev_seg: component bin_to_7seg
+	sev_seg0: component bin_to_7seg
 	port map (
-		input                          	=> internal_sig,                         
-		output 									=> output                  
+		input                          	=> internal_sig(3 downto 0),                         
+		output 									=> output0                  
   );
+  
+   sev_seg1: component bin_to_7seg
+	port map (
+		input                          	=> internal_sig(7 downto 4),                         
+		output 									=> output1                  
+  );
+  
+   sev_seg2: component bin_to_7seg
+	port map (
+		input                          	=> internal_sig(11 downto 8),                         
+		output 									=> output2                  
+  );
+  
 END ARCH_FAYARD_Lab2;
 
--- Software/app/main.c
+++ Software/app/main.c
@@ -1,20 +1,28 @@
 #include <system.h>
 #include <altera_avalon_pio_regs.h>
 
-int irq = 0, count_manual = 0;
+int count_manual = 0;
+int c0 = 0, c1 = 0, c2 = 0;
+int send;
 
 int main(int argc, char *argv[])
 {
     while(1){
-        if (count_manual < 9){
+        if (count_manual < 999){
             count_manual = count_manual + 1;
         }
         else {
             count_manual = 0;
         }
 
+        c2 = count_manual / 100;
+        c1 = (count_manual /10) % 10;
+        c0 = count_manual % 10;
+
+        send = (c2 << 8) + (c1 <<4) + c0;
+
         //Write the number on the 7 segment
-        IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,count_manual);
+        IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,send);
         usleep(100000);
 	}
 
